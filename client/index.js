41439cbe7b7b27d08e8bdadfd4aaa6966dab5613
irosen419 <irosen419@gmail.com>
2021-01-07 19:34:41 -0500

    successfully create songs and push to root page

--- client/index.js
+++ client/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
+import App from './components/App'
 import Songlist from './components/SongList'
+import SongCreate from './components/SongCreate'
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <Songlist />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Songlist} />
+        </Route>
+        <Route path="songs/new" component={SongCreate}>
+
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
